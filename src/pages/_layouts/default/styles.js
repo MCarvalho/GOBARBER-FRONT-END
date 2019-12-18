98897ebfb7a4de078ae332ccd054be0ad13d6d94
MCarvalho <178carvalho@gmail.com>
2019-12-18 19:37:06 -0300

    loading auth e toasts

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: rgb(113, 89, 193);
+  background: rgb(226, 202, 250);
   background: linear-gradient(
-    90deg,
-    rgba(113, 89, 193, 1) 0%,
-    rgba(171, 89, 193, 1) 100%
+    135deg,
+    rgba(253, 202, 250, 1) 0%,
+    rgba(103, 0, 184, 1) 100%
   );
 `;
