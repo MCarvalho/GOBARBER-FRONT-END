2a09001b4a12233aa370e5abe4fb83489891e3ea
MCarvalho <178carvalho@gmail.com>
2019-12-17 23:57:32 -0300

    Inicio FRONT - Preparacao

---
+++ src/pages/_layouts/auth/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: rgb(226, 202, 250);
+  background: linear-gradient(
+    90deg,
+    rgba(253, 202, 250, 1) 0%,
+    rgba(103, 0, 184, 1) 100%
+  );
+`;
---
+++ src/pages/_layouts/default/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: rgb(113, 89, 193);
+  background: linear-gradient(
+    90deg,
+    rgba(113, 89, 193, 1) 0%,
+    rgba(171, 89, 193, 1) 100%
+  );
+`;
